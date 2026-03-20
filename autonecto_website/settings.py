a26b57906f8a282082a11314a1f974ca5d543655
Tadiwanashe Justice <Tadiwanashe Justice@DESKTOP-BRBSKFE>
2026-03-20 18:01:25 +0100

    Fix deployment issues (static, migrate, secret key)

--- autonecto_website/settings.py
+++ autonecto_website/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)$dj@h^82704(q1zyn#(j+iv5kg5pgt2fs^!rig_bifovz7&se'
-
+SECRET_KEY = "django-insecure-test-key"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    "autonecto-website.onrender.com"
+]
 
 
 # Application definition
